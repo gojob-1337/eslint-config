2f22a2e84a7cf53983e4a875144e8626679d1e2c
VinceOPS <vince.g.135@gmail.com>
2019-08-01 10:30:47 +0200

    feat: disable "no-use-before-define"

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,12 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+## [1.0.4] - 2019-08-01
+
+### Changed
+
+- `@typescript-eslint/no-use-before-define` disabled 🤸
+
 ## [1.0.3] - 2019-08-01
 
 ### Fixed
--- typescript.js
+++ typescript.js
@@ -16,6 +16,7 @@ module.exports = {
     "@typescript-eslint/explicit-member-accessibility": "off",
     "@typescript-eslint/no-unused-vars": "error",
     "@typescript-eslint/no-parameter-properties": "off",
-    "@typescript-eslint/no-explicit-any": "off"
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/no-use-before-define": "off"
   }
 };
