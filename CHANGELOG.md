8d8e33f222adfa65b5bbc083a94e5b9eefe5e266
VinceOPS <vince.g.135@gmail.com>
2019-09-21 15:17:10 +0200

    fix: allow unused var when prefixed with _

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,12 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+## [1.0.9] - 2019-09-21
+
+### Changed
+
+- allow "unused var" if name is starting with `_`
+
 ## [1.0.8] - 2019-09-21
 
 ### Added
--- typescript.js
+++ typescript.js
@@ -15,7 +15,7 @@ module.exports = {
     "@typescript-eslint/explicit-function-return-type": "off",
     "@typescript-eslint/prefer-interface": "off",
     "@typescript-eslint/explicit-member-accessibility": "off",
-    "@typescript-eslint/no-unused-vars": "error",
+    "@typescript-eslint/no-unused-vars": ["error", { "argsIgnorePattern": "^_" }],
     "@typescript-eslint/no-parameter-properties": "off",
     "@typescript-eslint/no-explicit-any": "off",
     "@typescript-eslint/no-use-before-define": "off",
