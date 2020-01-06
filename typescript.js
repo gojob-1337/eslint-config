a5a66e37c01bdbdae66a1e6f5f3155a6206ac350
Emilien Escalle <emilien-ext@gojob.com>
2020-01-06 18:30:24 +0100

    feat: add eslint-plugin-import rules

--- typescript.js
+++ typescript.js
@@ -3,22 +3,43 @@ module.exports = {
     "plugin:@typescript-eslint/recommended",
     "prettier",
     "plugin:prettier/recommended",
-    "prettier/@typescript-eslint"
+    "prettier/@typescript-eslint",
+    "plugin:import/errors",
+    "plugin:import/warnings",
+    "plugin:import/typescript"
   ],
   parser: "@typescript-eslint/parser",
   plugins: ["@typescript-eslint"],
   rules: {
     semi: ["error", "always"],
-    "sort-imports": ["warn", { "ignoreCase": true, "ignoreDeclarationSort": true }],
+    "sort-imports": ["warn", { ignoreCase: true, ignoreDeclarationSort: true }],
     "prettier/prettier": ["error", { singleQuote: true }],
     "@typescript-eslint/indent": ["error", 2],
     "@typescript-eslint/explicit-function-return-type": "off",
     "@typescript-eslint/prefer-interface": "off",
     "@typescript-eslint/explicit-member-accessibility": "off",
-    "@typescript-eslint/no-unused-vars": ["error", { "argsIgnorePattern": "^_" }],
+    "@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
     "@typescript-eslint/no-parameter-properties": "off",
     "@typescript-eslint/no-explicit-any": "off",
     "@typescript-eslint/no-use-before-define": "off",
-    "@typescript-eslint/no-non-null-assertion": "off"
+    "@typescript-eslint/no-non-null-assertion": "off",
+    "import/order": [
+      "warn",
+      {
+        groups: ["builtin", "external", ["parent", "sibling", "index"]],
+
+        pathGroups: [
+          {
+            pattern: "@nestjs/**",
+            group: "external",
+            position: "before"
+          }
+        ],
+        "newlines-between": "always"
+        // alphabetize: {
+        //   order: "asc"
+        // }
+      }
+    ]
   }
 };
