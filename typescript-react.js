02d7ef1e41e47cb8dcfbbaa2c1a051cde477d6bb
Emilien Escalle <neilime@users.noreply.github.com>
2020-01-07 18:16:39 +0100

    Add eslint-plugin-import rules (#1)
    feat: eslint-plugin-import rules

--- typescript-react.js
+++ typescript-react.js
@@ -5,7 +5,10 @@ module.exports = {
     "prettier",
     "plugin:prettier/recommended",
     "prettier/@typescript-eslint",
-    "prettier/react"
+    "prettier/react",
+    "plugin:import/errors",
+    "plugin:import/warnings",
+    "plugin:import/typescript"
   ],
   parser: "@typescript-eslint/parser",
   plugins: ["@typescript-eslint", "react-hooks"],
@@ -17,13 +20,32 @@ module.exports = {
     "@typescript-eslint/explicit-function-return-type": "off",
     "@typescript-eslint/prefer-interface": "off",
     "@typescript-eslint/explicit-member-accessibility": "off",
-    "@typescript-eslint/no-unused-vars": ["error", { "argsIgnorePattern": "^_" }],
+    "@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
     "@typescript-eslint/no-parameter-properties": "off",
     "@typescript-eslint/no-explicit-any": "off",
     "@typescript-eslint/no-use-before-define": "off",
     "@typescript-eslint/no-non-null-assertion": "off",
     "react-hooks/rules-of-hooks": "error",
-    "react-hooks/exhaustive-deps": "warn"
+    "react-hooks/exhaustive-deps": "warn",
+    "import/order": [
+      "warn",
+      {
+        groups: ["builtin", "external", ["parent", "sibling", "index"]],
+
+        pathGroups: [
+          {
+            pattern: "react",
+            group: "external",
+            position: "before"
+          }
+        ],
+        "newlines-between": "always"
+        // TODO: alphabetize issue, waiting for new relase (https://github.com/benmosher/eslint-plugin-import/pull/1562)
+        // alphabetize: {
+        //   order: "asc"
+        // }
+      }
+    ]
   },
   settings: {
     react: {
